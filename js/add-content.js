dd355a63fe62c973e4cefc898c2ccd53859204e7
Art 3870-02 <art3870-02@Arts-194-109.local>
2018-04-25 15:54:31 -0700

    Fixing JS

--- js/add-content.js
+++ js/add-content.js
@@ -13,4 +13,27 @@ if (hourNow > 18) {//6:00 PM
 } else {
   greeting = 'Welcome!';
 }
-document.write('<h3>' + greeting + '</h3>');
+// document.write('<h3>' + greeting + '</h3>');
+document.getElementById('greeting').innerHTML=greeting;
+
+
+// Slide Show Review //
+var i = 0;
+var images = [];
+var time = 1000;
+
+images[0] = 'img/award_01.jpg';
+images[1] = 'img/award_02.jpg';
+images[2] = 'img/award_03.jpg';
+
+function changeImg(){
+  document.slide.src = images[i];
+
+if(i < images.length - 1){
+  i++;
+} else {
+  i = 0;
+}
+setTimeout("changeImg()", 3000);
+}
+window.onload = changeImg;
