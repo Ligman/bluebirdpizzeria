ee2a5f458264c26759cad6f1d9cd74a67654967c
Ligman <37819221+Ligman@users.noreply.github.com>
2018-05-01 17:04:52 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,1 @@
-$(document).foundation();
-var today = new Date();
-console.log(today);
-console.log(today.getHours());
-var hourNow = today.getHours();
-var greeting;
 
-if (hourNow > 18) {//6:00 PM
- greeting = 'Good evening!';
-} else if (hourNow > 12) { //12:00 PM
- greeting = 'Good afternoon!';
-} else if (hourNow > 0) { //12:00 Midnight
- greeting = 'Good morning!';
-} else {
- greeting = 'Welcome';
-}
-document.write('<h3>' + greeting +'</h3>');
